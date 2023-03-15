e2b47843f3f56aeb6228b2e8b943e9dbff1801d5
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-15 22:34:21 +0100

    update

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-const colors = require('colors');
+// class MyEmitter {
+//     constructor() {
+//         this.events = {};
+//     }
+//     on(event, listener) {
+//         this.events[event] = this.events[event] || [];
+//         this.events[event].push(listener);
+//     }
+//     emit(event) {
+//         if (this.events[event]) {
+//             this.events[event].forEach(listener => listener());
+//         }
 
-console.log('Hello WOOAOOOOAOOOOAOOOOAOAOWOOW'.rainbow);
+//     }
+// }
+
+const MyEmitter = require("events");
+const emitter = new MyEmitter();
+
+emitter.on("FILE_READ", (filename) => {
+  console.log("file was read:", filename);
+});
+
+emitter.on("FILE_READ", () => {
+  console.log("should cache file");
+});
+
+emitter.emit("FILE_READ", "filename");
