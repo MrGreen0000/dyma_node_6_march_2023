088f1d0374f39ce28e43f60348ae1a93ce1e674d
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-04-12 15:39:14 +0200

    update

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const fs = require("fs");
 fs.open("./fichiertest.txt", "a+", (err, fd) => {
   if (err) throw err;
   console.log(fd);
-  fs.close(fd, (err) => {
-    if (err) throw err;
+  fs.write(fd, "bonjour123", (err, written, str) => {
+    console.log({ err, written, str });
+    fs.close(fd, (err) => {
+      if (err) throw err;
+    });
   });
 });
