c9895a589e64bc7876734f55566600e8472f9f9b
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-04-12 16:45:31 +0200

    update

--- app.js
+++ app.js
@@ -1,31 +1,6 @@
-// const fs = require("fs");
-
-// fs.open("./fichiertest.txt", "a+", (err, fd) => {
-//   if (err) throw err;
-//   console.log(fd);
-//   fs.write(fd, "bonjour123", (err, written, str) => {
-//     console.log({ err, written, str });
-//     fs.close(fd, (err) => {
-//       if (err) throw err;
-//     });
-//   });
-// });
-
-const buffer = Buffer.alloc(5);
-
-console.log(buffer);
-// <Buffer 00 00 00 00 00>
-
 const fs = require("fs");
 
-fs.open("./test.txt", "w", (err, fd) => {
+fs.readFile("./fichiertest.txt", (err, data) => {
   if (err) throw err;
-  const buf1 = Buffer.from("têst");
-  fs.write(fd, buf1, (err, written, buffer) => {
-    if (err) throw err;
-    console.log(err, written, buffer);
-    fs.close(fd, () => {
-      console.log("Done");
-    });
-  });
+  console.log(data);
 });
