240f8831d3e4b0792b665938d1d40f1da157b8e6
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-05-10 16:17:17 +0200

    update

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const fs = require("fs");
+const http = require("http");
 
-fs.mkdir("./mondossier", (err) => {
-  if (err) throw err;
-  console.log("Le dossier à été créé !");
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  res.writeHead(200, {
+    "content-type": "text/plain",
+  });
+
+  res.end("got a response");
 });
+
+server.listen(8080);
