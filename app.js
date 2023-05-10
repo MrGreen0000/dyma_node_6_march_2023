7d59f63835aada62d88cf1d119c2864bbe5ead0c
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-05-10 16:58:45 +0200

    update

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const http = require("http");
-
-const server = http.createServer();
-
-server.on("request", (req, res) => {
-  res.writeHead(200, {
-    "content-type": "text/plain",
-  });
-
-  res.end("got a response");
-});
-
-server.listen(8080);
+http
+  .createServer((req, res) => {
+    const url = req.url;
+    if (url === "/home") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>Page d'accueil<h1>");
+      res.end();
+    } else if (url === "/contact") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>Contactez nous<h1>");
+      res.end();
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end();
+    }
+  })
+  .listen(3000);
